63c05941183f55f24ccb8ff73e09f92ca83840da
Allen Wild <allenwild93@gmail.com>
2018-01-20 02:12:09 -0500

    yocto-patches: improve apply-patches script
     * Try to find TOP (where .repo is) automatically
     * Add option to clear and reset branch and re-patch

--- yocto-patches/apply-patches.sh
+++ yocto-patches/apply-patches.sh
@@ -9,11 +9,26 @@ run() {
     "$@"
 }
 
-[[ -d $TOP ]] || die "Please set TOP first"
+gettop() {
+    local repodir=$PWD
+    while true; do
+        if [[ -d $repodir/.repo ]]; then
+            echo "Found TOP dir $repodir" >&2
+            echo "$repodir"
+            return 0
+        elif [[ $repodir == / ]]; then
+            echo "Unable to set TOP automatically: No .repo found" >&2
+            return 1
+        fi
+        repodir=$(dirname $repodir)
+    done
+}
+
+[[ -d $TOP ]] || TOP="$(gettop)" || die "Please set TOP first"
 thisdir=$(dirname $0)
 branchname=wild
 
-while read -r dir; do
+for dir in $(find "$thisdir" -mindepth 1 -type d -printf '%P\n'); do
     sdir=$(readlink -e $thisdir/$dir)
     pdir="$TOP/$dir"
     if [[ ! -d "$pdir" ]]; then
@@ -21,8 +36,20 @@ while read -r dir; do
         continue
     fi
     if git -C $pdir show-ref --heads | grep -q " refs/heads/$branchname\$"; then
-        echo "ERROR: branch $branchname already exists in $pdir"
-        continue
+        echo "WARNING: branch $branchname already exists in $pdir"
+        if [[ -n "$(git -C $pdir status --porcelain)" ]]; then
+            echo "ERROR: uncommited changes in $pdir. Skipping..."
+            continue
+        fi
+        echo -n "reset branch $branchname? (y/N) "
+        read answer
+        if [[ $answer == [yY]* ]]; then
+            repo sync -ld $pdir
+            git -C $pdir branch -D $branchname
+        else
+            echo "Skipping $pdir..."
+            continue
+        fi
     fi
     repo start $branchname $pdir || die "unable to create branch $branchname in $pdir"
     echo "Created branch $branchname in $pdir"
@@ -30,4 +57,4 @@ while read -r dir; do
         echo "Applying $(basename $patch)"
         git -C "$pdir" am -q --whitespace=nowarn "$patch" || die "failed to apply patch $patch"
     done
-done < <(find "$thisdir" -mindepth 1 -type d -printf '%P\n')
+done
