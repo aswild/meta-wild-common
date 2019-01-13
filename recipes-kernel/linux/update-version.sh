19677613c9796c022b15cf0687fe16781ae1e5ed
Allen Wild <allenwild93@gmail.com>
2019-01-13 00:30:16 -0500

    linux-mainline: update-version: commit afterwards
    Sync with update-srcrev.sh in meta-newbs

--- recipes-kernel/linux/update-version.sh
+++ recipes-kernel/linux/update-version.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+if [[ $1 == -c || $1 == --commit ]]; then
+    COMMIT=y
+    shift
+fi
 
-for MAJVER in 4.14 4.19; do
-    ver=$(./get-latest-version.py $MAJVER)
-    echo "$ver"
-    sed -i "s|^LINUX_VERSION.*|LINUX_VERSION = \"$ver\"|" linux-mainline_${MAJVER}.bb
+kvers=${1:-4.14 4.19}
+commitmsg='linux-mainline: bump to '
+bbfiles_commit=()
+
+for kver in $kvers; do
+    linux_version=$(./get-latest-version.py $kver)
+
+    bbfile=linux-mainline_${kver}.bb
+    oldversion=$(perl -ne 'print if s/^LINUX_VERSION\s*=\s*"(.*)"/\1/' $bbfile)
+    if [[ $oldversion != $linux_version ]]; then
+        commitmsg+="$linux_version, "
+        bbfiles_commit+=($bbfile)
+    fi
+
+    sed -i -e "s/^LINUX_VERSION.*/LINUX_VERSION = \"${linux_version}\"/" \
+           -e "s/^SRCREV.*/SRCREV = \"${rev}\"/" \
+           $bbfile
+
+    echo "set version to ${linux_version}"
 done
+
+if [[ $COMMIT == y && -n $bbfiles_commit ]]; then
+    commitmsg="${commitmsg%, }"
+    git commit -m "$commitmsg"  "${bbfiles_commit[@]}"
+fi
