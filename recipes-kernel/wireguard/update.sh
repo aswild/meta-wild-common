aba0e51b59ab5ad01b7707845e1b37abcd385f7c
Allen Wild <allenwild93@gmail.com>
2019-12-29 17:46:15 -0500

    wireguard: rewrite update script to handle split repos
    Drop the simple shell script and replace it with some Python.

--- recipes-kernel/wireguard/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-url='https://github.com/WireGuard/WireGuard'
-tag="$(git ls-remote --tags $url | grep -v '\^{}$' | awk -F/ '{print $NF}' | sort | tail -n1)"
-
-sed -i "s/^TAG.*/TAG = \"$tag\"/" wireguard.inc
-echo "Updated to tag $tag"
